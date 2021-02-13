f80ea6e3656aa5aa21fc95cb6f5462401ee92434
coderHook <coderhookdev@gmail.com>
2021-02-13 08:15:38 +0100

    Add cypress tests

---
+++ cypress/integration/pokedex.js
@@ -0,0 +1,14 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('can navigate to the pokemon page', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('charmander').click()
+    cy.url('http://localhost:5000/pokemon/charmander')
+    })
+  
+})
